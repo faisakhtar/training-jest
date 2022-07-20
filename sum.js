2361acbea451a08a068f38304a5627fee3dc60c1
faisakhtar <faisakhtar@gmail.com>
2022-07-20 23:26:09 +0100

    Add difference fn

--- sum.js
+++ sum.js
@@ -1,4 +1,7 @@
 function sum(a, b) {
     return a + b;
-  }
-  module.exports = sum;
+}
+function difference(a, b) {
+    return a - b;
+}
+module.exports ={ sum, difference};
--- sum.test.js
+++ sum.test.js
@@ -1,9 +1,17 @@
-const sum = require('./sum');
+const sums = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+    expect(sums.sum(1, 2)).toBe(3);
 });
 
 test('adds 3 + 3 to equal 6', () => {
-    expect(sum(3, 3)).toBe(6)
-  });
+    expect(sums.sum(3, 3)).toBe(6)
+});
+
+test('remove 10 - 5 to equal 5', () => {
+    expect(sums.difference(10, 5)).toBe(5)
+});
+
+
+
+
