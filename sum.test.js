7845029f136c276eaca450348b5fe3303837a7f1
faisakhtar <faisakhtar@gmail.com>
2022-07-20 23:21:32 +0100

    initial commit with basic tests

---
+++ sum.test.js
@@ -0,0 +1,9 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 3 + 3 to equal 6', () => {
+    expect(sum(3, 3)).toBe(6)
+  });
